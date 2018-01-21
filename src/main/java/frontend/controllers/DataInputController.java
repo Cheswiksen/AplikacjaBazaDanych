c222ade098836281065e2c814e00012322b9ef7e
Bartosz Czerwiński <bartosz.czerwinski@hotmail.com>
2018-01-21 18:04:44 +0100

    -przed dosages

--- src/main/java/frontend/controllers/DataInputController.java
+++ src/main/java/frontend/controllers/DataInputController.java
@@ -1,7 +1,9 @@
 package frontend.controllers;
 
 import backend.entities.AnimalsEntity;
+import backend.entities.DrugsEntity;
 import backend.entities.UsersEntity;
+import backend.utils.CollisionData;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
@@ -28,26 +30,65 @@ public class DataInputController {
     @FXML
     ComboBox AnimalComboBox;
     @FXML
-    public void OnStart()
+    ComboBox Drug1ComboBox;
+    @FXML
+    ComboBox Drug2ComboBox;
+    @FXML
+    ComboBox Drug3ComboBox;
+    @FXML
+    public void OnStartAnimal()
     {
+        AnimalComboBox.getItems().clear();
         List<AnimalsEntity> zwierzaki = getAnimals();
-
-        AnimalComboBox.getItems().addAll(zwierzaki);
+        for (AnimalsEntity a : zwierzaki)
+        {
+            AnimalComboBox.getItems().add(a.getAnimalName());
+        }
 
     }
+    @FXML
+    public void OnStartDrug1()
+    {
+        Drug1ComboBox.getItems().clear();
+        List<DrugsEntity> drugs = getDrugs();
+        for (DrugsEntity a : drugs)
+        {
+            Drug1ComboBox.getItems().add(a.getDrugName());
+        }
+    }
+    @FXML
+    public void OnStartDrug2()
+    {
+        Drug2ComboBox.getItems().clear();
+        List<DrugsEntity> drugs = getDrugs();
+        for (DrugsEntity a : drugs)
+        {
+            Drug2ComboBox.getItems().add(a.getDrugName());
+        }
+    }
+    @FXML
+    public void OnStartDrug3()
+    {
+        Drug3ComboBox.getItems().clear();
+        List<DrugsEntity> drugs = getDrugs();
+        for (DrugsEntity a : drugs)
+        {
+            Drug3ComboBox.getItems().add(a.getDrugName());
+        }
+    }
 
+    @FXML
+    public void CheckCollisionsAndDosages()
+    {
 
+    }
     public void setMediator(Mediator mediator) {
         this.mediator = mediator;
     }
 
 
 
-    public void getDrugs(Event event) {
-    }
 
-    public void CheckCollisionsAndDosages(ActionEvent actionEvent) {
-    }
 
     public void setUser(UsersEntity user) {
         this.user = user;
